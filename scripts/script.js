3fff1dfdd9dc386c2d9ed619f96e1379e63818bd
Prodix <prodixfg@gmial.com>
2022-11-12 22:05:34 +0300

    a

---
+++ scripts/script.js
@@ -0,0 +1,13 @@
+function myFunc() {
+    let el = document.getElementsByName("radiogroup");
+    let art = document.getElementsByClassName("rb");
+
+    for (var i = 0; i < 3; i++){
+        if (el[i].checked == true) {
+            art[i].style.borderColor = "#0B7572";
+        }
+        else {
+            art[i].style.borderColor = "transparent";
+        }
+    }
+}
